fea145ccbd302c429080565bc3c586d84bcf2e8f
XyzCoder <106374961+YounisXyz@users.noreply.github.com>
2024-03-15 06:33:53 +0500

    Update Insta.py

--- Insta.py
+++ Insta.py
@@ -1,6 +1,10 @@
-import os
+import os,time
 os.system("git pull")
 os.system("clear")
+print("\033[1;96mSubscribe Me On YouTube Channel For Keep Sporting and Updating 😍❤️‍🩹🤡 ")
+time.sleep(2)
+os.system("xdg-open https://www.youtube.com/@YounisXyz")
+os.system("clear") 
 print("""\033[0;97mInstgram feature has been add in XYZ offical tool
 please use XYZ Command!""")
 
@@ -17,4 +21,3 @@ python Xyz.py""")
 print("\033[0;97mCopy This Command and Run")
 print("\033[0;97mContact to Admin on WhatsApp For any Error: +923404708884")
 exit("\033[1;92m Thanks For Using YounisXyz Tools ❤️💀")
- 
