61b811476d5eac98c2834e18626c9b309d550164
XyzCoder <106374961+YounisXyz@users.noreply.github.com>
2024-03-15 06:30:11 +0500

    Update Insta.py

--- Insta.py
+++ Insta.py
@@ -1,7 +1,20 @@
-import os, sys
+import os
 os.system("git pull")
-try:
-    __import__("INSTA").YOUNISXD()
-except Exception as e:
-    exit(str(e))
+os.system("clear")
+print("""\033[0;97mInstgram feature has been add in XYZ offical tool
+please use XYZ Command!""")
+
+print("\033[1;93m XYZ installation Command 👇💀")
+
+print("""\033[1;92m
+cd
+clear
+rm -rf Xyz
+git clone https://github.com/YounisXyz/Xyz
+cd Xyz
+python Xyz.py""")
+
+print("\033[0;97mCopy This Command and Run")
+print("\033[0;97mContact to Admin on WhatsApp For any Error: +923404708884")
+exit("\033[1;92m Thanks For Using YounisXyz Tools ❤️💀")
  
